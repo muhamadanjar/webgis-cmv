5b6fe6e0e774e20fbbf537c0ce10fd19e2b8f6db
Muhamad Anjar <arvanria@gmail.com>
2016-11-09 11:47:56 +0700

    update external

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -6,7 +6,7 @@
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-        <title>SIMPOSI</title>
+        <title>WEBGIS CMV</title>
         <link rel="stylesheet" type="text/css" href="//js.arcgis.com/3.12compact/esri/css/esri.css">
         <link rel="stylesheet" type="text/css" href="{{ asset('vendor/fontawesome/css/font-awesome.min.css')}}">
         <link rel="stylesheet" type="text/css" href="{{ asset('cmv/css/theme/dbootstrap/dbootstrap.css')}}">
