c1098c4aa10105153e6c8a61947e182d362a2150
Muhamad Anjar <arvanria@gmail.com>
2016-11-28 14:35:11 +0700

    update webgis

--- app/Http/Controllers/SettingWebCtrl.php
+++ app/Http/Controllers/SettingWebCtrl.php
@@ -75,7 +75,149 @@ class SettingWebCtrl extends Controller {
 
 
 	public function getGantiprofil($value=''){
-		return 'ganti profil';
+		$users = \Auth::user();
+		return view('settings.gantiprofil')->withUsers($users);
+	}
+
+	public function postGantiprofil(Request $request){
+
+		$fileName = str_random(20) . '.' . $request->file('image')->getClientOriginalExtension();	
+
+		$users = \App\User::find(\Auth::user()->id);
+		$users->name = $request->name;
+		$users->email = $request->email;
+		$users->instansi = $request->instansi;
+		$users->no_telp = $request->no_telp;
+		$users->jenis_kebutuhan = $request->jenis_kebutuhan;
+		
+		if($fileName != null){
+			$this->UploadFile($request->file('image'),$fileName);
+			$users->image = $fileName;
+		}
+
+		if($request->oldpassword == $request->password){
+			$users->password = $request->oldpassword;		
+		}else{
+			$users->password = bcrypt($request->password);			
+		}
+		$users->save();
+		return redirect('admin/setting/gantiprofil');
+	}
+
+	function UploadFile($fupload,$fileName){
+        //direktori file
+        $destinationPath = public_path('files');
+        if (!file_exists($destinationPath)) {
+            mkdir($destinationPath, 0777);
+            echo "The directory $destinationPath was successfully created.";
+            exit;
+        } else {
+            echo "The directory $destinationPath exists.";
+        }
+       
+        $fuploadName = $fupload->getClientOriginalName();
+        $fuploadExt = $fupload->getClientOriginalExtension();
+        $fuploadSize = $fupload->getSize();
+        //$fileName = str_random(20) . '.' . $fupload->getClientOriginalExtension();   
+        //Simpan file
+        $fupload->move($destinationPath, $fileName);
+       
+    }
+
+	public function getVisitor(Request $request){
+		$statistik = new Statistik();
+		$ip      = $statistik->ip_user();
+		$browser = $statistik->browser_user();
+		$os      = $statistik->os_user();
+		$visitor = $request->cookie('VISITOR');
+		$tanggal = date("Ymd");
+		$waktu = time();
+		$bataswaktu = $waktu - 300;
+		// Check bila sebelumnya data pengunjung sudah terrekam
+		if (!isset( $visitor )) {
+		
+			// Masa akan direkam kembali
+			// Tujuan untuk menghindari merekam pengunjung yang sama dihari yang sama.
+			// Cookie akan disimpan selama 24 jam
+			$duration = time()+60*60*24;
+		
+			// simpan kedalam cookie browser
+			//setcookie('VISITOR',$browser,$duration);
+			cookie('VISITOR', $browser, $duration);
+		
+			// SQL Command atau perintah SQL INSERT
+			//$sql = "INSERT INTO statistik (ip, os, browser) VALUES ('$ip', '$os', '$browser')";
+			$statistik = DB::table('statistik_web')->orderBy('online','DESC')->where('ip',$ip)->where('date_create',$tanggal)->get();
+			if (count($statistik) > 0){
+				
+				DB::table('statistik_web')
+				->where('ip', $ip)->where('date_create', $tanggal)
+				->update(
+					[
+						'hits' => $statistik[0]->hits + 1, 
+						'online' => $waktu
+					]
+				);
+			}else{
+				DB::table('statistik_web')->insert(
+					[
+						'ip' => $ip, 
+						'os' => $os,
+						'hits' => 1, 
+						'browser' => $browser,
+						'online' => $waktu,
+						'date_create' => Carbon::now()
+					]
+				);
+			}
+			
+
+			// variabel { $db } adalah perwakilan dari koneksi database lihat config.php
+			//$query = $db->query($sql);
+			return redirect()->intended($this->redirectPathVisitor());
+		}
+		
+	}
+
+	public function setCookie(Request $request){
+      $minutes = 1;
+      $response = new Response('Hello World');
+      $response->withCookie(cookie('name', 'virat', $minutes));
+      return $response;
+   }
+   public function getCookie(Request $request){
+      $value = $request->cookie('name');
+      echo $value;
+   }
+
+   public function getStatistiklist($value=''){
+	   $statistik = DB::table('statistik_web')->get();
+
+	   return view('master.statistikList')->withStatistik($statistik);
+   }
+
+
+
+   public function redirectPathVisitor(){
+        if (property_exists($this, 'redirectPathVisitor')){
+            return $this->redirectPathVisitor;
+        }
+
+        return property_exists($this, 'redirectToVisitor') ? $this->redirectToVisitor : '/map';
+    }
+
+	public function getPengunjungonline($value=''){
+		$bataswaktu = time() - 300;
+		$pengunjungonline = \DB::table('statistik_web')->where('online','>',$bataswaktu)->get();
+		return count($pengunjungonline);
+	}
+
+	public function getHitstoday($value=''){
+		
+		$hits = \DB::table('statistik_web')->select('SUM(hits as hitstoday)')->where('date_create',$tanggal)
+		->groubBy('date_create')->get();
+
+		return $hits;
 	}
 
 
--- public/simposi.js
+++ public/simposi.js
@@ -1,3 +1,35 @@
+function numeralsOnly(evt) {
+    evt = (evt) ? evt : event;
+    var charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode :
+    ((evt.which) ? evt.which : 0));
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+            alert("Hanya Nomor yang bisa di input pada kolom ini.");
+            return false;
+        }
+    return true;
+}
+
+function lettersOnly(evt) {
+    evt = (evt) ? evt : event;
+    var charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode :
+        ((evt.which) ? evt.which : 0));
+    if (charCode > 31 && (charCode < 65 || charCode > 90) && (charCode < 97 || charCode > 122)) {
+        alert("Enter letters only.");
+        return false;
+    }
+    return true;
+}
+
+function ynOnly(evt) {
+    evt = (evt) ? evt : event;
+    var charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode : ((evt.which) ? evt.which : 0));
+    if (charCode > 31 && charCode != 78 && charCode != 89 && charCode != 110 && charCode != 121) {
+    alert("Enter \"Y\" or \"N\" only.");
+    return false;
+    }
+    return true;
+}
+
 (function($, window, document){
     $('.formConfirm').on('click', function(e) {
         e.preventDefault();
